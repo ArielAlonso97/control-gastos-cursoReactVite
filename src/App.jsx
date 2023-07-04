5f1f7d3d574bc8c1930d8d7946cf682d19348ccf
Ariel <ariel32_124@hotmail.com>
2023-07-04 17:32:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
+
+import Header from "./components/Header";
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  return (
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="Icono nuevo gasto" 
+          onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
